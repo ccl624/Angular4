2ce63041603733e4d87202eab6ec79659f93916e
ccl624 <ccl851764651@icloud.com>
2017-11-20 23:10:18 +0800

    属性和事件绑定

--- 项目测试/angular-demo/src/app/my-world/my-world.component.ts
+++ 项目测试/angular-demo/src/app/my-world/my-world.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-my-world',
   templateUrl: './my-world.component.html',
   styleUrls: ['./my-world.component.css']
 })
-export class MyWorldComponent implements OnInit {
-
+export class MyWorldComponent implements OnInit , OnChanges{
+  username:string = 'zhangwuji';
+  fullName:string = 'zhaomin';
   constructor() { }
 
   ngOnInit() {
+    
   }
-
+  changeName(){
+    this.username = 'zhouzhiruo';
+  }
+  ngOnChanges(){
+    console.log(this.fullName);
+  }
 }
